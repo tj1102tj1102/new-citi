da9e96e309330f7d35ecaf1228ea62c7666254f1
sunshine-web-coder <tj1102tj1102@gmail.com>
2026-01-16 10:18:34 +0100

    Fdfd

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -11,7 +11,8 @@ export const users: User[] = [
     email: 'sarah.johnson@email.com',
     phone: '(555) 123-4567',
     transactionCode: '6363',
-    transactionMsg: "The recipient's bank account could not be verified. Please double-check the account number and routing number, then try again.",
+    transactionMsg:
+      "The recipient's bank account could not be verified. Please double-check the account number and routing number, then try again.",
     createdAt: '2023-01-15',
     accounts: [
       {
@@ -130,7 +131,7 @@ export const users: User[] = [
         issuer: 'Visa',
         isPrimary: true,
         createdAt: '2022-06-15'
-      },
+      }
     ]
   },
   {
@@ -143,7 +144,8 @@ export const users: User[] = [
     createdAt: '2026-1-13', // y-m-d
     updatedAt: '2026-1-15', // y-m-d
     transactionCode: '7894',
-    transactionMsg: 'You cannot make transactions right now. Kindly drop Anti money certificate to withdraw your funds.',
+    transactionMsg:
+      'You cannot make transactions right now. Kindly drop Anti money certificate to withdraw your funds.',
     accounts: [
       {
         type: 'checking',
@@ -153,14 +155,16 @@ export const users: User[] = [
         isPrimary: true,
         transactions: [
           {
-            merchant: 'Transfer from Alice Dan:Inheritance',
+            merchant:
+              'Transfer from Alice Dan:Inheritance',
             category: '****7463',
             date: 'Jan 18, 2026',
             amount: 100000000.0,
             status: 'success'
           },
           {
-            merchant: 'Transfer from Alice Dan:Inheritance',
+            merchant:
+              'Transfer from Alice Dan:Inheritance',
             category: '****4389',
             date: 'Jan 12, 2026',
             amount: 150000000.0,
@@ -172,7 +176,7 @@ export const users: User[] = [
             date: 'Jan 12, 2026',
             amount: 1000.0,
             status: 'success'
-          },
+          }
         ]
       },
       {
@@ -180,7 +184,7 @@ export const users: User[] = [
         name: 'High Yield Savings',
         accountNumber: '0987654321',
         balance: 0.0,
-        isPrimary: false,
+        isPrimary: false
       }
     ],
     cards: [
@@ -194,7 +198,7 @@ export const users: User[] = [
         issuer: 'Visa',
         isPrimary: true,
         createdAt: '2021-09-10'
-      },
+      }
     ]
   },
   {
@@ -207,7 +211,8 @@ export const users: User[] = [
     username: 'Jessicawgood56',
     password: '1122lovemeAa$',
     transactionCode: '7894',
-    transactionMsg: 'Your account is on hold. You cannot make transactions right now. Please contact our customer service team for assistance.',
+    transactionMsg:
+      'Your account is on hold. You cannot make transactions right now. Please contact our customer service team for assistance.',
     accounts: [
       {
         type: 'checking',
@@ -218,7 +223,7 @@ export const users: User[] = [
         isPrimary: true,
         transactions: [
           {
-            merchant: 'Cash Deposit Payment',
+            merchant: 'Peace Keeping Disbursement',
             category: '****4389',
             date: 'Jan 5, 2026',
             amount: 9800090.0,
@@ -230,7 +235,7 @@ export const users: User[] = [
             date: 'December 28, 2025',
             amount: 0.0,
             status: 'success'
-          },
+          }
         ]
       },
       {
@@ -239,7 +244,7 @@ export const users: User[] = [
         accountNumber: '4589021736',
         routingNumber: '073915842',
         balance: 0.0,
-        isPrimary: false,
+        isPrimary: false
       }
     ],
     cards: [
@@ -253,7 +258,157 @@ export const users: User[] = [
         issuer: 'Visa',
         isPrimary: true,
         createdAt: '2021-09-10'
+      }
+    ]
+  },
+  {
+    id: '0004',
+    firstName: 'Chris',
+    lastName: 'Hughes',
+    email: 'c**@gmail.com',
+    createdAt: '2026-1-16', // y-m-d
+    username: 'ChrisHug56',
+    password: 'Chris1204',
+    transactionCode: '7894',
+    transactionMsg:
+      'Your account is on hold. You cannot make transactions right now. Please contact our customer service team for assistance.',
+    accounts: [
+      {
+        type: 'checking',
+        name: 'Everyday Checking',
+        accountNumber: '42008334486',
+        routingNumber: '354731724',
+        balance: 4355290.0,
+        isPrimary: true,
+        transactions: [
+          {
+            merchant: 'Peace Keeping Disbursement',
+            category: '****4721',
+            date: 'Jan 5, 2026',
+            amount: 400000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Military Payroll Credit',
+            category: '****6389',
+            date: 'Dec 22, 2025',
+            amount: 350000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Direct Deposit',
+            category: '****2910',
+            date: 'Nov 18, 2025',
+            amount: 320000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Trip Expenses Reimbursement',
+            category: '****8543',
+            date: 'Oct 10, 2025',
+            amount: 300000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Peace Keeping Disbursement',
+            category: '****1124',
+            date: 'Sep 28, 2025',
+            amount: 275000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Military Payroll Credit',
+            category: '****5698',
+            date: 'Aug 14, 2025',
+            amount: 265000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Direct Deposit',
+            category: '****9872',
+            date: 'Jul 3, 2025',
+            amount: 250000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Trip Expenses Reimbursement',
+            category: '****3345',
+            date: 'Jun 21, 2025',
+            amount: 240000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Peace Keeping Disbursement',
+            category: '****7781',
+            date: 'May 17, 2025',
+            amount: 225000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Military Payroll Credit',
+            category: '****5566',
+            date: 'Apr 6, 2025',
+            amount: 200000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Direct Deposit',
+            category: '****9933',
+            date: 'Mar 29, 2025',
+            amount: 190000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Trip Expenses Reimbursement',
+            category: '****2211',
+            date: 'Feb 12, 2025',
+            amount: 180000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Peace Keeping Disbursement',
+            category: '****6677',
+            date: 'Jan 23, 2025',
+            amount: 175000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Military Payroll Credit',
+            category: '****4455',
+            date: 'Jan 8, 2025',
+            amount: 180000.0,
+            status: 'success'
+          },
+          {
+            merchant: 'Direct Deposit',
+            category: '****8899',
+            date: 'Jan 1, 2025',
+            amount: 200290.0,
+            status: 'success'
+          }
+        ]
       },
+      {
+        type: 'savings',
+        name: 'High Yield Savings',
+        accountNumber: '4589021736',
+        routingNumber: '073915842',
+        balance: 0.0,
+        isPrimary: false
+      }
+    ],
+    cards: [
+      {
+        id: 'card_003',
+        cardNumber: '4927348162059473',
+        expiryDate: '08/27',
+        cvv: '789',
+        cardType: 'debit',
+        cardName: 'Premier Checking Card',
+        issuer: 'Visa',
+        isPrimary: true,
+        createdAt: '2021-09-10'
+      }
     ]
   }
 ];
